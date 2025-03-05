8af5e2b762a67301765aa9699d717f18dde419b8
Christopher Alphonse <>
2025-03-04 21:38:59 -0500

    Enhance PowerShell profile with PSFzf module and Chocolatey tab-completion support

--- pwsh/Microsoft.PowerShell_profile.ps1
+++ pwsh/Microsoft.PowerShell_profile.ps1
@@ -1,6 +1,8 @@
 Import-Module Terminal-Icons
+Import-Module PSFzf
 
 
+oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\sonicboom_dark.omp.json" | Invoke-Expression
 
 [console]::InputEncoding = [console]::OutputEncoding = New-Object System.Text.UTF8Encoding
 
@@ -50,10 +52,7 @@ if (-not (Get-Module -Name PSFzf -ListAvailable)) {
 }
 Set-PsFzfOption -PSReadlineChordProvider 'Ctrl+t' -PSReadlineChordReverseHistory 'Ctrl+r'
 
-# Customize Oh-My-Posh
-if (Test-Path 'C:\Users\chris-desktop\AppData\Local\Programs\oh-my-posh\themes\blue-owl.omp.json') {
-    oh-my-posh init pwsh --config 'C:\Users\chris-desktop\AppData\Local\Programs\oh-my-posh\themes\blue-owl.omp.json' | Invoke-Expression
-}
+
 
 function admin {
     Start-Process wt.exe -Verb RunAs
@@ -174,3 +173,13 @@ Set-Alias grep findstr
 
 }
 lazy-load-utilities
+
+# Import the Chocolatey Profile that contains the necessary code to enable
+# tab-completions to function for `choco`.
+# Be aware that if you are missing these lines from your profile, tab completion
+# for `choco` will not function.
+# See https://ch0.co/tab-completion for details.
+$ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
+if (Test-Path($ChocolateyProfile)) {
+  Import-Module "$ChocolateyProfile"
+}
