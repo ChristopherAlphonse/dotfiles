7e2983fa22a43209f82ee75464e392989e3a200c
Christopher Alphonse <christopheralphonse96@gmail.com>
2025-05-10 00:21:08 -0400

    Update .wezterm.lua

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -1,34 +1,187 @@
--- Pull in the wezterm API
-local wezterm = require("wezterm")
 
--- This will hold the configuration.
+
+local wezterm = require("wezterm")
+local act = wezterm.action
+
 local config = wezterm.config_builder()
 
--- This is where you actually apply your config choices
+config.front_end = "OpenGL"
+config.max_fps = 60
+config.default_cursor_style = "BlinkingBlock"
+config.animation_fps = 1
+config.cursor_blink_rate = 500
+config.term = "xterm-256color"
 
 config.font = wezterm.font("MesloLGS Nerd Font Mono")
+
+config.cell_width = 0.9
+
+config.window_background_opacity = 0.9
+config.prefer_egl = true
 config.font_size = 16
 
-config.enable_tab_bar = false
-
-config.window_decorations = "RESIZE"
-config.window_background_opacity = 0.8
-config.macos_window_background_blur = 10
-
-config.default_prog = { "pwsh" }
-
--- my coolnight colorscheme:
-config.colors = {
-	foreground = "#CBE0F0",
-	background = "#011423",
-	cursor_bg = "#47FF9C",
-	cursor_border = "#47FF9C",
-	cursor_fg = "#011423",
-	selection_bg = "#033259",
-	selection_fg = "#CBE0F0",
-	ansi = { "#214969", "#E52E2E", "#44FFB1", "#FFE073", "#0FC5ED", "#a277ff", "#24EAF7", "#24EAF7" },
-	brights = { "#214969", "#E52E2E", "#44FFB1", "#FFE073", "#A277FF", "#a277ff", "#24EAF7", "#24EAF7" },
+config.window_padding = {
+	left = 10,
+	right = 10,
+	top = 10,
+	bottom = 10,
 }
 
--- and finally, return the configuration to wezterm
+-- tabs
+config.hide_tab_bar_if_only_one_tab = true
+config.use_fancy_tab_bar = false
+
+wezterm.on("toggle-colorscheme", function(window, pane)
+	local overrides = window:get_config_overrides() or {}
+	if overrides.color_scheme == "Zenburn" then
+		overrides.color_scheme = "Cloud (terminal.sexy)"
+	else
+		overrides.color_scheme = "Zenburn"
+	end
+	window:set_config_overrides(overrides)
+end)
+
+-- keymaps
+config.keys = {
+ -- Terminal split horizontally (equivalent to `workbench.action.terminal.split`)
+    {
+      key = '\\',
+      mods = 'CTRL',
+      action = wezterm.action.SplitHorizontal { domain = 'CurrentPaneDomain' },
+    },
+
+    {
+      key = '[',
+      mods = 'CTRL',
+      action = wezterm.action.ActivatePaneDirection 'Left',
+    },
+
+
+    {
+      key = ']',
+      mods = 'CTRL',
+      action = wezterm.action.ActivatePaneDirection 'Right',
+    },
+
+    -- Open a new tab
+    {
+      key = 'n',
+      mods = 'CTRL',
+      action = wezterm.action.SpawnTab 'CurrentPaneDomain',
+    },-- Go to the next tab
+{
+  key = "Tab",
+  mods = "CTRL",
+  action = wezterm.action.ActivateTabRelative(1),
+},
+
+-- Go to the previous tab
+{
+  key = "Tab",
+  mods = "CTRL|SHIFT",
+  action = wezterm.action.ActivateTabRelative(-1),
+},
+
+    -- Open new window
+    {
+      key = 'f',
+      mods = 'CTRL',
+      action = wezterm.action.SpawnWindow,
+    },
+
+    -- Scroll up and down by line values
+    {
+      key = 'UpArrow',
+      mods = 'CTRL',
+      action = wezterm.action.ScrollByLine(-10),
+    },
+    {
+      key = 'DownArrow',
+      mods = 'CTRL',
+      action = wezterm.action.ScrollByLine(5),
+    },
+
+	{
+		key = "I",
+		mods = "CTRL|SHIFT",
+		action = wezterm.action.SplitPane({
+			direction = "Up",
+			size = { Percent = 50 },
+		}),
+	},
+	{
+		key = "K",
+		mods = "CTRL|SHIFT",
+		action = wezterm.action.SplitPane({
+			direction = "Down",
+			size = { Percent = 50 },
+		}),
+	},
+
+	{ key = "9", mods = "CTRL", action = act.PaneSelect },
+	{ key = "L", mods = "CTRL", action = act.ShowDebugOverlay },
+	{
+		key = "O",
+		mods = "CTRL|ALT",
+		-- toggling opacity
+		action = wezterm.action_callback(function(window, _)
+			local overrides = window:get_config_overrides() or {}
+			if overrides.window_background_opacity == 1.0 then
+				overrides.window_background_opacity = 0.9
+			else
+				overrides.window_background_opacity = 1.0
+			end
+			window:set_config_overrides(overrides)
+		end),
+	},
+}
+
+-- For example, changing the color scheme:
+config.color_scheme = "Cloud (terminal.sexy)"
+config.colors = {
+
+	background = "#0c0b0f",
+
+	cursor_border = "#bea3c7",
+
+	cursor_bg = "#bea3c7",
+
+
+	tab_bar = {
+		background = "#0c0b0f",
+
+		active_tab = {
+			bg_color = "#0c0b0f",
+			fg_color = "#bea3c7",
+			intensity = "Normal",
+			underline = "None",
+			italic = false,
+			strikethrough = false,
+		},
+		inactive_tab = {
+			bg_color = "#0c0b0f",
+			fg_color = "#f8f2f5",
+			intensity = "Normal",
+			underline = "None",
+			italic = false,
+			strikethrough = false,
+		},
+
+		new_tab = {
+			bg_color = "#0c0b0f",
+			fg_color = "white",
+		},
+	},
+}
+
+config.window_frame = {
+	font = wezterm.font({ family = "Iosevka Custom", weight = "Regular" }),
+	active_titlebar_bg = "#47FF9C",
+
+}
+
+config.window_decorations = "NONE | RESIZE"
+config.default_prog = { "pwsh", "-NoLogo" }
+config.initial_cols = 80
+
 return config
