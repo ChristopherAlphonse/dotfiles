e120428701149e2a270d47305d5a4487a7c9e84a
Christopher Alphonse <christopheralphonse96@gmail.com>
2025-09-14 16:25:44 -0400

    Modify wezterm configuration with new settings
    Updated font and color scheme settings. Added custom key bindings for pane management.

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -1,34 +1,41 @@
--- Pull in the wezterm API
-local wezterm = require("wezterm")
 
--- This will hold the configuration.
+local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
--- This is where you actually apply your config choices
 
-config.font = wezterm.font("MesloLGS Nerd Font Mono")
+config.font = wezterm.font("BlexMono Nerd Font Mono")
 config.font_size = 16
-
-config.enable_tab_bar = false
-
+config.color_scheme = "tokyonight_night"
+config.colors={
+    cursor_bg="#7aa2f7",
+    cursor_border="#7aa2f7",
+}
+config.default_cursor_style = "BlinkingBar"
 config.window_decorations = "RESIZE"
 config.window_background_opacity = 0.8
 config.macos_window_background_blur = 10
-
 config.default_prog = { "pwsh" }
 
--- my coolnight colorscheme:
-config.colors = {
-	foreground = "#CBE0F0",
-	background = "#011423",
-	cursor_bg = "#47FF9C",
-	cursor_border = "#47FF9C",
-	cursor_fg = "#011423",
-	selection_bg = "#033259",
-	selection_fg = "#CBE0F0",
-	ansi = { "#214969", "#E52E2E", "#44FFB1", "#FFE073", "#0FC5ED", "#a277ff", "#24EAF7", "#24EAF7" },
-	brights = { "#214969", "#E52E2E", "#44FFB1", "#FFE073", "#A277FF", "#a277ff", "#24EAF7", "#24EAF7" },
+
+config.keys={{
+    key = "q",
+    mods = "ALT",
+    action = wezterm.action.CloseCurrentPane { confirm = false },
+},
+{
+    key = "\\",
+    mods = "CTRL",
+    action = wezterm.action.SplitHorizontal { domain = "CurrentPaneDomain" },},
+ {
+    key = "[",
+    mods = "CTRL",
+    action = wezterm.action.ActivatePaneDirection "Left",
+  },  {
+    key = "]",
+    mods = "CTRL",
+    action = wezterm.action.ActivatePaneDirection "Right",
+  },
 }
 
--- and finally, return the configuration to wezterm
+
 return config
